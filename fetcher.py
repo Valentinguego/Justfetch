73364199355befebf85bf5da1ee66962683fb6aa
Valentinguego <guegovalentin63@gmail.com>
2026-04-17 13:59:11 +0000

    url_fixed

--- fetcher.py
+++ fetcher.py
@@ -6,4 +6,11 @@ load_dotenv()
 
 API_KEY = os.getenv("WATCHMODE_API_KEY")
 
-  
+netflix_id = 203
+pays = ["IS", "AU", "GB"]
+
+for region in pays:
+    url = f"https://api.watchmode.com/v1/list-titles/?apiKey={API_KEY}&source_ids={netflix_id}&regions={region}"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
